ea2dbb95d6ad7c7f1b83c5f057269e1bad4597b8
Kelsey Garcia <kelseyigarcia@gmail.com>
2021-01-07 11:05:25 -0700

    Editable project header and colors

---
+++ UltimatePortfolio/ProjectHeaderView.swift
@@ -0,0 +1,36 @@
+//
+//  ProjectHeaderView.swift
+//  UltimatePortfolio
+//
+//  Created by Kelsey Garcia on 1/7/21.
+//
+
+import SwiftUI
+
+struct ProjectHeaderView: View {
+    @ObservedObject var project: Project
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(project.projectTitle)
+                
+                ProgressView(value: project.completionAmount)
+                    .accentColor(Color(project.projectColor))
+            }.padding(.bottom, 10)
+            
+            Spacer()
+            
+            NavigationLink(destination: EmptyView()) {
+                Image(systemName: "square.and.pencil")
+                    .imageScale(.large)
+            }
+        }
+    }
+}
+
+struct ProjectHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectHeaderView(project: Project.example)
+    }
+}
