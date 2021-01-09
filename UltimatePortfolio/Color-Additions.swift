d2ea4564c4c5374e45dc31ba011d5b7b6c2d4735
Kelsey Garcia <kelseyigarcia@gmail.com>
2021-01-09 16:03:33 -0700

    Improved home page

---
+++ UltimatePortfolio/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  UltimatePortfolio
+//
+//  Created by Kelsey Garcia on 1/9/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
--- UltimatePortfolio/HomeView.swift
+++ UltimatePortfolio/HomeView.swift
@@ -6,22 +6,108 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct HomeView: View {
     static let tag: String? = "Home"
+    let items: FetchRequest<Item>
+    
+    @FetchRequest(entity: Project.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Project.title, ascending: true)], predicate: NSPredicate(format: "closed = false")) var projects: FetchedResults<Project>
     
     @EnvironmentObject var dataController: DataController
+    
+    init() {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate = NSPredicate(format: "completed = false")
+        
+        request.sortDescriptors = [
+            NSSortDescriptor(keyPath: \Item.priority, ascending: false)
+        ]
+        
+        request.fetchLimit = 10
+        
+        items = FetchRequest(fetchRequest: request)
+    } // end init
+    
+    var projectRows: [GridItem] {
+        [GridItem(.fixed(100))]
+    } // end projectRows
+    
     var body: some View {
         NavigationView {
-            VStack {
-                Button("Add Data") {
-                    dataController.deleteAll()
-                    try? dataController.createSampleData()
-                }
-                .navigationTitle("Home")
+            ScrollView {
+                VStack(alignment: .leading) {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHGrid(rows: projectRows) {
+                            ForEach(projects) { project in
+                                VStack(alignment: .leading) {
+                                    Text("\(project.projectItems.count) items")
+                                        .font(.caption)
+                                        .foregroundColor(.secondary)
+                                    
+                                    Text(project.projectTitle)
+                                        .font(.title2)
+                                    
+                                    ProgressView(value: project.completionAmount)
+                                        .accentColor(Color(project.projectColor))
+                                } // end VStack
+                                .padding([.horizontal, .top])
+                                .background(Color.secondarySystemGroupedBackground)
+                                .cornerRadius(10)
+                                .shadow(color: Color.black.opacity(0.2), radius: 5)
+                            } // end ForEach
+                        } // end LazyHGrid
+                        .fixedSize(horizontal: false, vertical: true)
+                    } // end ScrollView
+                    VStack(alignment: .leading) {
+                        list("Up next", for: items.wrappedValue.prefix(3))
+                        list("More to explore", for: items.wrappedValue.dropFirst(3))
+                    } // end VStack
+                    .padding(.horizontal)
+                } // end VStack
+            } // end ScrollView
+            .background(Color.systemGroupedBackground.ignoresSafeArea())
+            .navigationTitle("Home")
+        } // end NavigationView
+    } // end body
+    
+    @ViewBuilder func list(_ title: String, for items: FetchedResults<Item>.SubSequence) -> some View {
+        if items.isEmpty {
+            EmptyView()
+        } else {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.secondary)
+                .padding(.top)
+            
+            ForEach(items) { item in
+                NavigationLink(destination: EditItemView(item: item)) {
+                    HStack(spacing: 20) {
+                        Circle()
+                            .stroke(Color(item.project?.projectColor ?? "Light Blue"), lineWidth: 3)
+                            .frame(width: 44, height: 44)
+                        
+                        VStack(alignment: .leading) {
+                            Text(item.itemTitle)
+                                .font(.title2)
+                                .foregroundColor(.primary)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            
+                            if item.itemDetail.isEmpty == false {
+                                Text(item.itemDetail)
+                                    .foregroundColor(.secondary)
+                            }
+                        } // end VStack
+                    } // end HStack
+                    .padding()
+                    .background(Color.secondarySystemGroupedBackground)
+                    .cornerRadius(10)
+                    .shadow(color: Color.black.opacity(0.2), radius: 5)
+                } // end NavigationLink
             }
         }
-    }
+    } // end list
+    
 }
 
 struct HomeView_Previews: PreviewProvider {
